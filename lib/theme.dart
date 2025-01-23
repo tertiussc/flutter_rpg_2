9067481800165704d470ae50060d53ea87ae09ce
Tertius Scheepers <tertiussc@gmail.com>
2025-01-23 08:24:52 +0200

    added color palette

---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = Color.fromRGBO(162, 29, 19, 1);
+  static Color primaryAccent = Color.fromRGBO(120, 14, 14, 1);
+  static Color secondaryColor = Color.fromRGBO(45, 45, 45, 1);
+  static Color secondaryAccent = Color.fromRGBO(35, 35, 35, 1);
+  static Color titleColor = Color.fromRGBO(200, 200, 200, 1);
+  static Color textColor = Color.fromRGBO(150, 150, 150, 1);
+  static Color successColor = Color.fromRGBO(9, 149, 110, 1);
+  static Color highlightColor = Color.fromRGBO(212, 172, 13, 1);
+}
